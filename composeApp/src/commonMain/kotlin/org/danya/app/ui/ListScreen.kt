df0369cefe38ac906be5cde5d015f3b40d5a8e33
Danylo Zakusylo <danzakusilo@gmail.com>
2023-12-29 09:58:18 +0200

    initial

---
+++ composeApp/src/commonMain/kotlin/org/danya/app/ui/ListScreen.kt
@@ -0,0 +1,11 @@
+package org.danya.app.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ListScreen() {
+    LazyColumn {
+
+    }
+}
---
+++ composeApp/src/jsMain/kotlin/org/danya/app/App.js.kt
@@ -0,0 +1,7 @@
+package org.danya.app
+
+import kotlinx.browser.window
+
+internal actual fun openUrl(url: String?) {
+    url?.let { window.open(it) }
+}
