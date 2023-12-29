557196543e6069d0834a927729051015f9b4b7aa
Danylo Zakusylo <danzakusilo@gmail.com>
2023-12-29 09:58:18 +0200

    initial

---
+++ composeApp/src/commonMain/kotlin/org/danya/app/ui/searchList/ListScreenViewModel.kt
@@ -0,0 +1,5 @@
+package org.danya.app.ui.searchList
+
+class ListScreenViewModel(){
+
+}
---
+++ composeApp/src/jvmMain/kotlin/org/danya/app/preview/ListPreview.kt
@@ -0,0 +1,11 @@
+package org.danya.app.preview
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import org.danya.app.ui.ListScreen
+
+@Preview
+@Composable
+fun ListPreview(){
+    ListScreen()
+}
