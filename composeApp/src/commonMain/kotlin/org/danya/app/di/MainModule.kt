b5cc39652d80e5acf97a6bd9fbca0a0bb5f2ea3a
Danylo Zakusylo <danzakusilo@gmail.com>
2024-01-22 22:10:47 +0200

    add ktor module

--- composeApp/src/commonMain/kotlin/org/danya/app/di/MainModule.kt
+++ composeApp/src/commonMain/kotlin/org/danya/app/di/MainModule.kt
@@ -1,7 +1,20 @@
 package org.danya.app.di
 
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.Logging
 import org.koin.dsl.module
 
 internal val mainModule = module {
 
+}
+
+
+val ktorClientModule = module {
+    single<HttpClient> {
+        HttpClient {
+            install(Logging)
+            install(ContentNegotiation)
+        }
+    }
 }
--- composeApp/src/jvmMain/kotlin/org/danya/app/preview/ListPreview.kt
+++ composeApp/src/jvmMain/kotlin/org/danya/app/preview/ListPreview.kt
@@ -2,7 +2,7 @@ package org.danya.app.preview
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
-import org.danya.app.ui.ListScreen
+import org.danya.app.ui.searchList.ListScreen
 
 @Preview
 @Composable
